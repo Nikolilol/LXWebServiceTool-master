d2962d5d1060c8b8f63b04ca90563cfd0b6a1c4b
Nikolilol <niko@NikodeMacBook-Air.local>
2016-05-16 09:12:16 +0800

    version1.1
    use Class method to instead Object method

--- LXWebServiceTool/ViewController.m
+++ LXWebServiceTool/ViewController.m
@@ -9,67 +9,99 @@
 #import "ViewController.h"
 #import "LXWebServiceTool.h"
 
-@interface ViewController ()<LXWebServiceToolDelegate>
-
+@interface ViewController ()
+@property(nonatomic, weak) LXWebServiceTool *tool;
+@property(nonatomic, weak) UILabel *titleLabel;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
-    [self requestWebService];
+    [self setupView];
+}
+
+- (void)setupView{
+    UILabel *titleLabel = [UILabel new];
+    titleLabel.frame = CGRectMake(0, 100, self.view.frame.size.width, 100);
+    titleLabel.text = @"中国";
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.textColor = [UIColor blackColor];
+    titleLabel.font = [UIFont fontWithName:@"AppleGothic" size:20.0];
+    _titleLabel = titleLabel;
+    [self.view addSubview:titleLabel];
+    
+    titleLabel.backgroundColor = [UIColor redColor];
+    
+    UIButton *toTraditionalChinese = [[UIButton alloc] initWithFrame:CGRectMake(10, 210, (self.view.frame.size.width - 30 )/2 , 60)];
+    [toTraditionalChinese setTitle:@"toTraditionalChinese" forState:UIControlStateNormal];
+    [toTraditionalChinese addTarget:self action:@selector(toTraditionalChineseClick:) forControlEvents:UIControlEventTouchUpInside];
+    toTraditionalChinese.backgroundColor = [UIColor grayColor];
+    [self.view addSubview:toTraditionalChinese];
+    
+    UIButton *toSimplifiedChinese = [[UIButton alloc] initWithFrame:CGRectMake(toTraditionalChinese.frame.size.width + 20, 210, toTraditionalChinese.frame.size.width, 60)];
+    [toSimplifiedChinese setTitle:@"toSimplifiedChinese" forState:UIControlStateNormal];
+    [toSimplifiedChinese addTarget:self action:@selector(toSimplifiedChineseClick:) forControlEvents:UIControlEventTouchUpInside];
+    toSimplifiedChinese.backgroundColor = [UIColor grayColor];
+    [self.view addSubview:toSimplifiedChinese];
 }
 
 //for example:
 /*
-POST /WebServices/MobileCodeWS.asmx HTTP/1.1
-Host: webservice.webxml.com.cn
-Content-Type: application/soap+xml; charset=utf-8
-Content-Length: length
-
-<?xml version="1.0" encoding="utf-8"?>
-<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-<soap12:Body>
-<getMobileCodeInfo xmlns="http://WebXml.com.cn/">
-<mobileCode>string</mobileCode>
-<userID>string</userID>
-</getMobileCodeInfo>
-</soap12:Body>
-</soap12:Envelope>
-
-HTTP/1.1 200 OK
-Content-Type: application/soap+xml; charset=utf-8
-Content-Length: length
-
-<?xml version="1.0" encoding="utf-8"?>
-<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-<soap12:Body>
-<getMobileCodeInfoResponse xmlns="http://WebXml.com.cn/">
-<getMobileCodeInfoResult>string</getMobileCodeInfoResult>
-</getMobileCodeInfoResponse>
-</soap12:Body>
-</soap12:Envelope>
+ SOAP 1.2
+ 
+ 以下是 SOAP 1.2 请求和响应示例。所显示的占位符需替换为实际值。
+ 
+ POST /WebServices/TraditionalSimplifiedWebService.asmx HTTP/1.1
+ Host: www.webxml.com.cn
+ Content-Type: application/soap+xml; charset=utf-8
+ Content-Length: length
+ 
+ <?xml version="1.0" encoding="utf-8"?>
+ <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+ <soap12:Body>
+ <toTraditionalChinese xmlns="http://webxml.com.cn/">
+ <sText>string</sText>
+ </toTraditionalChinese>
+ </soap12:Body>
+ </soap12:Envelope>
+ HTTP/1.1 200 OK
+ Content-Type: application/soap+xml; charset=utf-8
+ Content-Length: length
+ 
+ <?xml version="1.0" encoding="utf-8"?>
+ <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+ <soap12:Body>
+ <toTraditionalChineseResponse xmlns="http://webxml.com.cn/">
+ <toTraditionalChineseResult>string</toTraditionalChineseResult>
+ </toTraditionalChineseResponse>
+ </soap12:Body>
+ </soap12:Envelope>
  */
-
-- (void)requestWebService
+- (void)toTraditionalChineseClick:(id)sender
 {
-    NSArray *parArray = [[NSArray alloc] initWithObjects:@"mobileCode", @"userID", @" ", @" ", nil];
-    [LXWebServiceTool requestWithTarget:self SoapMethod:@"getMobileCodeInfo" parArray:parArray webServiceUrl:[NSURL URLWithString: @"http://webservice.webxml.com.cn/WebServices/MobileCodeWS.asmx"]];
+    NSArray *parArray = @[@"sText", _titleLabel.text];
+    [LXWebServiceTool requestWithNameSpace:@"http://webxml.com.cn/" SoapMthod:@"toTraditionalChinese" parArray:parArray webServiceUrl:[NSURL URLWithString: @"http://www.webxml.com.cn/WebServices/TraditionalSimplifiedWebService.asmx"] success:^(NSString *methodName, id responseObject) {
+        NSLog(@"methodName = %@\nresponseObjecct = %@", methodName, responseObject);
+        _titleLabel.text = responseObject;
+    } errorBlock:^(NSString *methodName, NSError *error) {
+        
+        NSLog(@"methodName = %@\nerror = %@", methodName, error);
+        _titleLabel.text = [NSString stringWithFormat:@"methodName = %@\nerror = %@", methodName, error];
+    }];
 }
 
-# pragma mark - WebServiceToolDelegate
-- (void)WebServiceToolFailedWithXmlPar:(id)error
+- (void)toSimplifiedChineseClick:(id)sender
 {
-    //requst error
-    NSLog(@"%@", error);
+    NSArray *parArray = @[@"sText", _titleLabel.text];
+    [LXWebServiceTool requestWithNameSpace:@"http://webxml.com.cn/" SoapMthod:@"toSimplifiedChinese" parArray:parArray webServiceUrl:[NSURL URLWithString: @"http://www.webxml.com.cn/WebServices/TraditionalSimplifiedWebService.asmx"] success:^(NSString *methodName, id responseObject) {
+        NSLog(@"methodName = %@\nresponseObjecct = %@", methodName, responseObject);
+        _titleLabel.text = responseObject;
+    } errorBlock:^(NSString *methodName, NSError *error) {
+        
+        NSLog(@"methodName = %@\nerror = %@", methodName, error);
+        _titleLabel.text = [NSString stringWithFormat:@"methodName = %@\nerror = %@", methodName, error];
+    }];
 }
-
-- (void)WebServiceToolFinsihedWithXmlPar:(id)responseObj
-{
-    //requst successful
-    NSLog(@"%@",responseObj);
-}
-
 @end
--- LXWebServiceTool/WebServiceTool/LXWebServiceTool.h
+++ LXWebServiceTool/WebServiceTool/LXWebServiceTool.h
@@ -7,42 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
-@class LXWebServiceTool;
 
-@protocol LXWebServiceToolDelegate <NSObject>
+typedef void (^successBlock)(NSString *methodName, id responseObject);
+typedef void (^errorBlock)(NSString *methodName, NSError *error);
 
-@optional
-/**
- *  代理方法，成功返回数据时调用
- *
- *  @param responseObj 返回的数据
- */
-- (void)WebServiceToolFinsihedWithXmlPar:(id)responseObj;
-
-/**
- *  代理方法，失败返回数据时调用
- *
- *  @param error 错误原因
- */
-- (void)WebServiceToolFailedWithXmlPar:(id)error;
-@end
-
-@interface LXWebServiceTool : NSObject<NSXMLParserDelegate,  NSURLConnectionDelegate>
-@property (strong, nonatomic) NSMutableData *webData;
-@property (strong, nonatomic) NSMutableString *soapResults;
-@property (strong, nonatomic) NSXMLParser *xmlParser;
-@property (nonatomic) BOOL elementFound;
-@property (strong, nonatomic) NSString *matchingElement;
+@interface LXWebServiceTool : NSObject
 @property (strong, nonatomic) NSURLConnection *conn;
-@property (nonatomic, strong) id<LXWebServiceToolDelegate> delegate;
 
 /**
- *  webservice网络请求
+ *  request to webService
  *
- *  @param target 代理对象一般写self
- *  @param soapMethod webservice方法名
- *  @param matchinStr webservice匹配返回结果的关键字
- *  @param parArray   参数列表及参数的值, 顺序为所有的参数名+所有的参数值, 参数名顺序根据webservice上的顺序相同
+ *  @param nameSpace the name space of your WebService
+ *  @param soapMethod webservice method name
+ *  @param parArray webservice need paramil
+ *  @param webServiceUrl   the url of webservice
  */
-+ (void)requestWithTarget:(id)target SoapMethod:(NSString *)soapMethod parArray:(NSArray *)parArray webServiceUrl:(NSURL *)webServiceUrl;
++ (void)requestWithNameSpace:(NSString *)nameSpace SoapMthod:(NSString *)soapMethod parArray:(NSArray *)parArray webServiceUrl:(NSURL *)webServiceUrl success:(successBlock)success errorBlock:(errorBlock)errorBlock;
 @end
